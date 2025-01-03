0b2403b07a9f159a1d405ffa324702434321eb17
Rafael Luiz de Oliveira <luizrafaelcontato@gmail.com>
2025-01-02 23:28:32 -0300

    refactor: organização de funções lógicas

--- ts/index.ts
+++ ts/index.ts
@@ -1,6 +1,5 @@
 const form = document.querySelector("#searchForm > form");
-const input: HTMLInputElement | null =
-  document.querySelector("#inpLocalização");
+const input: HTMLInputElement | null = document.querySelector("#inpLocalização");
 const tempoInfo = document.querySelector("#tempoInfo");
 
 console.log(input);
@@ -15,30 +14,37 @@ form?.addEventListener("submit", async (event) => {
     alert("Digite uma localização válida");
     return;
   }
-  try{
+  try {
+    const dados = await fetchWeatherData(localização);
+    displayWeatherData(dados);
+  } catch (error) {
+    alert("Localização não encontrada!");
+    console.log("Erro: ", error);
+  }
+});
+
+async function fetchWeatherData(localização: string) {
   const response = await fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${localização}&appid=5db2cb028d7ad8fd287db19fd4df58be&lang=pt_br&units=metric`
   );
-  const dados = await response.json();
+  if (!response.ok) {
+    throw new Error("Erro ao buscar dados do tempo");
+  }
+  return response.json();
+}
 
-  console.log(dados);
-
+function displayWeatherData(dados: any) {
+  if (!tempoInfo) return; // Verificação adicional para garantir que tempoInfo não seja nulo
   const infos = {
     temperatura: `${Math.round(dados.main.temp)} °C`,
     local: dados.name,
     icone: `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`,
   };
   tempoInfo.innerHTML = `
-        <div class="tempoDados">
-            <h2>${infos.local}</h2>
-            <span>${infos.temperatura}</span>
-        </div>
-            
-        <img src="${infos.icone}" width="100" alt="Sol e nuvens">
-  
+    <div class="tempoDados">
+      <h2>${infos.local}</h2>
+      <span>${infos.temperatura}</span>
+    </div>
+    <img src="${infos.icone}" width="100" alt="Sol e nuvens">
   `;
-}catch(error){
-    alert("Localização não encontrada!");
-    console.log("Erro: ", error);
 }
-});
